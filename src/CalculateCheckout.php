147da51dc0d16f7be79842e92dda06a2820d3cc6
Gihovani Filipp Pereira Demetrio <gihovani@gmail.com>
2023-10-30 21:52:23 -0300

    Criacao do caso de uso calculate checkout

---
+++ src/CalculateCheckout.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace GihovaniDemetrio\BoasPraticas;
+
+class CalculateCheckout
+{
+    function execute($input): array
+    {
+        $db = new \mysqli('127.0.0.1', 'magento', 'magento', 'test');
+        $response = file_get_contents('https://localhost/boas-praticas/currencies.php', false, stream_context_create(['ssl' => ['verify_peer' => false, 'verify_peer_name' => false]]));
+        $currencies = json_decode($response, true);
+        $currency = $currencies[$input['currency']];
+        $subtotal = 0;
+        $freight = 0;
+        foreach ($input['items'] as $item) {
+            $productId = $item['productId'];
+            $query = 'SELECT * FROM test.product WHERE product_id = ?';
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('i', $productId);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $product = $result->fetch_assoc();
+            $amount = floatval($product['amount']);
+            $itemAmount = $item['quantity'] * $amount;
+            $subtotal += $itemAmount;
+        }
+
+        $taxes = 0;
+        $protection = 0.055706;
+        if ($subtotal && $input['country'] === 'BR') {
+            if ($subtotal + $freight > 50) {
+                $importTax = ($subtotal + $freight) * 0.60;
+                $icms = ($subtotal + $freight + $importTax) * 0.17;
+                $taxes = $importTax + $icms + ($subtotal * $protection);
+            } else {
+                $taxes = ($subtotal + $freight) * 0.17;
+            }
+        }
+
+
+        $total = $subtotal + $taxes + $freight;
+
+        $db->close();
+
+        return [
+            'subtotal' => round($subtotal * $currency, 2),
+            'taxes' => round($taxes * $currency, 2),
+            'total' => round($total * $currency, 2)
+        ];
+    }
+}
--- tests/CalculateCheckoutTest.php
+++ tests/CalculateCheckoutTest.php
@@ -1,13 +1,15 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class IndexTest extends TestCase
+final class CalculateCheckoutTest extends TestCase
 {
+    private \GihovaniDemetrio\BoasPraticas\CalculateCheckout $calculateCheckout;
     public function setUp(): void
     {
-        require_once __DIR__ . '/../index.php';
         parent::setUp();
+        $this->calculateCheckout = new \GihovaniDemetrio\BoasPraticas\CalculateCheckout();
     }
 
     /**
@@ -15,7 +17,7 @@ final class IndexTest extends TestCase
      */
     public function testMain(array $input, array $expected): void
     {
-        $result = main($input);
+        $result = $this->calculateCheckout->execute($input);
         $this->assertEquals($result, $expected);
     }
 
